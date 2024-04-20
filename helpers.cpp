e1964e970945cc8ba17d446214788ca3a7e3b44d
José Pablo Alpaca Rivera <jose.p.alpaca@gmail.com>
2024-04-19 21:19:50 -0500

    feat: Add some base class

---
+++ helpers.cpp
@@ -0,0 +1,11 @@
+#include "helpers.h"
+
+namespace utils {
+    std::string capitalize(std::string str) {
+        if (!str.empty()) {
+            str[0] = toupper(str[0]);
+            transform(str.begin() + 1, str.end(), str.begin() + 1, [](unsigned char c) { return tolower(c); });
+        }
+        return str;
+    }
+}
---
+++ helpers.h
@@ -0,0 +1,10 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+#include <string>
+
+namespace helpers {
+    std::string capitalize(std::string str);
+}
+
+#endif  // HELPERS_H
